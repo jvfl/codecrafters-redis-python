45ebe7cb2eb71465b76bd00a693d2151724bb495
jvfl <jvflcin@gmail.com>
2025-03-02 16:02:00 -0300

    respond to multiple pings

--- app/main.py
+++ app/main.py
@@ -5,9 +5,16 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-
     connection, _ = server_socket.accept()
-    connection.sendall(b"+PONG\r\n")
+
+    while True:
+        # Read the first command from the client.
+        client_message = connection.recv(4096)
+        print(f"Received message: {client_message}")
+        pings = [ping for ping in client_message.splitlines() if ping == b"PING"]
+
+        for _ in pings:
+            connection.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
