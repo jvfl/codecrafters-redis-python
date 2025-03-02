d97cbc23eadd1a2b4b18d2716c7164228026c59e
jvfl <jvflcin@gmail.com>
2025-03-02 16:56:36 -0300

    serve multiple connections with asyncio

--- app/main.py
+++ app/main.py
@@ -1,21 +1,39 @@
 import socket  # noqa: F401
+import asyncio  # noqa: F401
 
 
-def main():
+async def handle_callback(
+    reader: asyncio.StreamReader, writer: asyncio.StreamWriter
+) -> None:
     print("Logs from your program will appear here!")
 
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept()
-
     while True:
-        # Read the first command from the client.
-        client_message = connection.recv(4096)
-        print(f"Received message: {client_message}")
-        pings = [ping for ping in client_message.splitlines() if ping == b"PING"]
+        raw_command = await reader.read(100)
+        command = raw_command.decode()
+
+        if command == "":
+            break
+
+        pings = [ping for ping in command.splitlines() if ping == "PING"]
 
         for _ in pings:
-            connection.sendall(b"+PONG\r\n")
+            writer.write(b"+PONG\r\n")
+            await writer.drain()
+
+    writer.close()
+    await writer.wait_closed()
+
+
+async def run_server(host: str, port: int) -> None:
+    server = await asyncio.start_server(handle_callback, host, port)
+    async with server:
+        await server.serve_forever()
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        asyncio.run(run_server("localhost", 6379))
+    except KeyboardInterrupt:
+        print("\nShutting down server...")
+    except Exception as e:
+        print(f"Error running server: {e}")
